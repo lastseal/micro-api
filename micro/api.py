07e470b462af0f826547b7be57ab67bb4cbcb9b6
Rodrigo <rarconte@lastseal.com>
2024-06-11 22:34:53 -0400

    CHG: métodos HTTP

--- micro/api.py
+++ micro/api.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*
 
+import requests
+import socketio
 import os
 
 API_URL = os.getenv("API_URL")
@@ -10,6 +12,80 @@ APi_TOKEN = os.getenv("APi_TOKEN")
 
 class Client:
 
-    def __init__(self, url=API_URL, token=APi_TOKEN):
+    def __init__(self, url=API_URL, token=APi_TOKEN, timeout=None, websocket=False):
         self.url = url
-        self.token = token
+        self.timeout = timeout
+        self.sio = None
+
+        self.session = requests.Session()
+        self.session.headers.update({"Authorization": f"Bearer {token}"})
+        
+        if websocket:
+            self.sio = socketio.SimpleClient()
+            self.sio.connect(self.url, transports=['websocket'])
+
+    def get(self, uri, params=None):
+        
+        url = f"{self.url}{uri}"
+        res = self.session.get(url, params=params, timeout=self.timeout)
+
+        if res.status_code >= 400:
+            raise Exception({
+                "status": res.status_code, 
+                "message": res.text
+            })
+        
+        return res.json()
+    
+    def post(self, uri, data):
+
+        url = f"{self.url}{uri}"
+        res = self.session.post(url, json=data, timeout=self.timeout)
+
+        if res.status_code >= 400:
+            raise Exception({
+                "status": res.status_code, 
+                "message": res.text
+            })
+        
+        return res.json()
+    
+    def put(self, uri, data):
+
+        url = f"{self.url}{uri}"
+        res = self.session.put(url, json=data, timeout=self.timeout)
+
+        if res.status_code >= 400:
+            raise Exception({
+                "status": res.status_code, 
+                "message": res.text
+            })
+        
+        return res.json()
+    
+    def delete(self, uri, data):
+
+        url = f"{self.url}{uri}"
+        res = self.session.delete(url, timeout=self.timeout)
+
+        if res.status_code >= 400:
+            raise Exception({
+                "status": res.status_code, 
+                "message": res.text
+            })
+        
+        return res.json()
+    
+    def subscribe(self, name, criteria, event="POST"):
+        if self.sio is not None:
+            self.sio.emit("subscribe", {
+                "name": name,
+                "event": event,
+                "criteria": criteria
+            })
+        
+        def decorator(handle):
+            handle()
+
+        return decorator
+

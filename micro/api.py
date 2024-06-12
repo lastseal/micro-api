fe663a02cdcae4a676cbcfba90bb03b9939dd69c
Rodrigo Arriaza <rarriaza@lastseal.com>
2024-06-11 20:48:29 -0400

    Update api.py

--- micro/api.py
+++ micro/api.py
@@ -1,1 +1,15 @@
+# -*- coding: utf-8 -*
 
+import os
+
+API_URL = os.getenv("API_URL")
+APi_TOKEN = os.getenv("APi_TOKEN")
+
+##
+#
+
+class Client:
+
+    def __init__(self, url=API_URL, token=APi_TOKEN):
+        self.url = url
+        self.token = token
